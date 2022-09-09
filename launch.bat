c5bf63de1195b044437be820fead0b2f4230ecd0
LincolnVR <112900530+LincolnVR@users.noreply.github.com>
2022-09-09 03:12:28 -0700

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 @echo off
 echo "Installing requirements (be sure to have python installed and in PATH)"
 pip install -r requirements.txt
-python osc_counter/setup.py
+python3 osc_counter/setup.py
 pause
